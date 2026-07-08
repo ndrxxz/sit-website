756d0dfa7715f55317300fe4041dc94aaf61342a
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-07-08 17:41:00 +0800

    refactor routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,17 +1,19 @@
 import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 
-const Home = lazy(() => import("@/pages/Home"));
-const Services = lazy(() => import("@/pages/Services/Services"));
-const AboutUs = lazy(() => import("@/pages/AboutUs"));
+const Home = lazy(() => import("@/pages"));
+const Services = lazy(() => import("@/pages/Services"));
+const AboutUs = lazy(() => import("@/pages"));
+const NotFound = lazy(() => import("@/pages"))
 
 function AppRoutes() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        <Route path="/" elements={<Home />} />
-        <Route path="/services" elements={<Services />} />
-        <Route path="/about-us" elements={<AboutUs />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Suspense>
   );
