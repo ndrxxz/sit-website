39441a3cfff449f020674d3291daa848f96ad14d
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-07-09 15:25:55 +0800

    create site file

---
+++ src/config/site.js
@@ -0,0 +1,8 @@
+const site = {
+  url: "https://yourdomain.com", // TODO
+  companyName: "The Spark Info-Tech Enterprise",
+  description: "TODO: short company description",
+  logo: "/logo.png",
+};
+
+export default site;
