a0333ad3b8f2513f01f7257b20d2a8d84405169e
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-07-09 15:25:32 +0800

    create contact

---
+++ src/config/contact.js
@@ -0,0 +1,13 @@
+const contact = {
+  email: "info@yourdomain.com",
+  telephone: "+63-...",
+  address: {
+    streetAddress: "4F, Simkimban Bldg., 700 Taguig St., Rizal Village",
+    locality: "Makati",
+    region: "Metro Manila",
+    postalCode: "1208",
+    country: "PH",
+  },
+};
+
+export default contact;
