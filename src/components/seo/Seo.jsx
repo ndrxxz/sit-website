7b6628ce4daf72662bf1c4454e2f7da86a9dd4b2
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-07-09 15:25:01 +0800

    Create Seo

---
+++ src/components/seo/Seo.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Helmet } from "react-helmet-async";
+
+// interface SEOProps {
+//   title: string;
+//   description: string;
+//   canonical?: string;
+//   image?: string
+// }
+
+export default function Seo({ title, description, canonical, image }) {
+  return (
+    <Helmet>
+      <title>{title}</title>
+
+      <meta name="edscription" content={description} />
+
+      {image && <meta name="og:image" content={image} />}
+
+      <meta property="og:title" content={title} />
+
+      <meta property="og:description" content={description} />
+
+      {canonical && <link rel="canonical" href={canonical} />}
+    </Helmet>
+  );
+}
