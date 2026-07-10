9d4dfaa7fa346020b08c377e842b8ba967a54f6f
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-07-10 14:02:30 +0800

    Implement Error Boundary

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -5,6 +5,7 @@ import { PageLoader, Navigation } from "@/components";
 
 import useOnlineStatus from "@/hooks/useOnlineStatus";
 import NoInternetConnection from "@/pages/NoInternetConnection";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 const Home = lazy(() => import("@/pages/Home"));
 const Services = lazy(() => import("@/pages/Services"));
@@ -19,17 +20,19 @@ function AppRoutes() {
   }
 
   return (
-    <Suspense fallback={<PageLoader />}>
-      <Navigation />
-      <div className="max-w-5xl mx-auto p-4 pt-30">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </div>
-    </Suspense>
+    <ErrorBoundary>
+      <Suspense fallback={<PageLoader />}>
+        <Navigation />
+        <div className="max-w-5xl mx-auto p-4 pt-30">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </div>
+      </Suspense>
+    </ErrorBoundary>
   );
   
 }
