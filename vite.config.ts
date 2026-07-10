533e687c708104450f209e9a636caf1ed1dbc70c
keith3 <keith3soriano@gmail.com>
2026-07-10 10:59:07 +0800

    bugfix::removeObfuscator
    - remove obfuscator

--- vite.config.ts
+++ vite.config.ts
@@ -1,61 +1,33 @@
-
-import path from "path"
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
-import { createHtmlPlugin } from "vite-plugin-html";
+import { defineConfig }       from "vite"
+import tailwindcss            from "@tailwindcss/vite"
+import react                  from "@vitejs/plugin-react"
+import path                   from "path";
+import { createHtmlPlugin }   from "vite-plugin-html";
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
     createHtmlPlugin({
       minify: true,
     }),
-
-    command === "build"
-      ? obfuscatorPlugin({
-          compact: true,
-          controlFlowFlattening: true,
-          controlFlowFlatteningThreshold: 1,
-          deadCodeInjection: true,
-          deadCodeInjectionThreshold: 0.4,
-          debugProtection: true,
-          debugProtectionInterval: 2000,
-          disableConsoleOutput: true,
-          identifierNamesGenerator: "hexadecimal",
-          numbersToExpressions: true,
-          rotateStringArray: true,
-          selfDefending: true,
-          shuffleStringArray: true,
-          simplify: true,
-          splitStrings: true,
-          splitStringsChunkLength: 5,
-          stringArray: true,
-          stringArrayEncoding: ["base64"],
-          stringArrayThreshold: 1,
-        } as any)
-      : null,
-  ].filter(Boolean),
-
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-
   build: {
     cssMinify: true,
     sourcemap: false,
     minify: "terser",
     terserOptions: {
+      mangle: true,
       compress: {
         drop_console: true,
-        drop_debugger: true,
-      },
-      mangle: true,
-    },
-  },
-}));
+        drop_debugger: true
+      }
+    }
+  }
+});
