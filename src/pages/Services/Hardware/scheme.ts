5e67815d8faf38a4de1929f56fe70dc1ed73d537
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-07-13 11:25:12 +0800

    refactor:: hardware services

--- src/pages/Services/Hardware/scheme.ts
+++ src/pages/Services/Hardware/scheme.ts
@@ -1,43 +1,46 @@
 import { IconType } from "react-icons";
 
-import { MdComputer, MdMemory } from "react-icons/md";
-import { BsBoxSeam } from "react-icons/bs";
-import { PiBroomLight } from "react-icons/pi";
+import {
+  RiBrush2Line,
+  RiComputerLine,
+  RiCpuLine,
+  RiFileListLine,
+} from "react-icons/ri";
 
 export interface Sheme {
-    id: number;
-    title: string;
-    description: string;
-    icon: IconType;
+  id: number;
+  title: string;
+  description: string;
+  icon: IconType;
 }
 
-export const Scheme: Sheme[] =  [
-    {
-        id: 1,
-        title: "Custom PC Assembly",
-        description:
-          "Get a custom-built PC tailored to your specific needs—whether for gaming, business, graphic design, video editing, or everyday productivity. We use quality components for optimal performance and reliability.",
-        icon: MdComputer
-    },
-    {
-        id: 2,
-        title: "Component Upgrades",
-        description:
-          "Boost your computer's speed and efficiency with professional hardware upgrades. From SSDs and RAM to graphics cards and processors, we recommend and install the right components for your system.",
-        icon: MdMemory
-    },
-    {
-        id: 3,
-        title: "Inventory Management",
-        description:
-          "Keep track of your IT assets with organized hardware inventory management. We help businesses monitor equipment, manage warranties, and maintain accurate asset records for better operational efficiency.",
-        icon: BsBoxSeam
-    },
-    { 
-        id: 4,
-        title: "Deep Cleaning",
-        description:
-          "Improve cooling performance and extend your computer's lifespan with professional internal cleaning. We safely remove dust buildup, replace thermal paste when needed, and optimize airflow to prevent overheating.",
-        icon: PiBroomLight
-    }
+export const Scheme: Sheme[] = [
+  {
+    id: 1,
+    title: "Custom PC Assembly",
+    description:
+      "Component assembly and configuration for a fully functional system.",
+    icon: RiComputerLine,
+  },
+  {
+    id: 2,
+    title: "Component Upgrades",
+    description:
+      "Installing and updating hardware and device drivers to ensure proper functionality.",
+    icon: RiCpuLine,
+  },
+  {
+    id: 3,
+    title: "Inventory Management",
+    description:
+      "Tracking and listing all hardware components and network devices present in the site.",
+    icon: RiFileListLine,
+  },
+  {
+    id: 4,
+    title: "Deep Cleaning",
+    description:
+      "Cleaning devices to prevent dust buildup, lower temperatures, and protect laptop or computer parts.",
+    icon: RiBrush2Line,
+  },
 ];
