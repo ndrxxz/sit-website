ca6c4b933375b3f2a89d8f49140845ee9daf88e2
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-07-13 11:23:40 +0800

    refactor:: home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,13 +7,13 @@ function Home() {
       <Seo title="Home" description="" canonical="/" />
       <StructuredData data={""} />
       <div
-        className="w-screen relative left-1/2 right-1/2 mx-[-50vw] -mt-30 h-screen bg-cover bg-center bg-no-repeat"
+        className="w-screen relative left-1/2 right-1/2 mx-[-50vw] -mt-30 min-h-screen bg-cover bg-center bg-no-repeat"
         style={{ backgroundImage: `url(${heroImage})` }}
       >
-        <div className="flex items-center justify-center flex-col gap-10 min-h-250 px-6 py-12 md:px-12">
+        <div className="flex items-center justify-center flex-col gap-10 min-h-screen px-6 py-12 md:px-12">
           <header className="flex flex-col gap-4 leading-[100%] tracking-normal text-center max-w-3xl">
             <h1 className="font-medium text-6xl">
-              Your One-Stop Solution for All Your IT Needs
+              Igniting Every Aspect of Your Digital Growth.
             </h1>
             <p className="font-normal text-base">
               Building your hardware, securing your network, and developing the
@@ -23,7 +23,7 @@ function Home() {
 
           <BookCall />
 
-          <div className="w-full max-w-[800px] aspect-video bg-white rounded-[40px]"></div>
+          <div className="w-full max-w-200 aspect-video bg-white rounded-[40px]"></div>
         </div>
       </div>
     </>
